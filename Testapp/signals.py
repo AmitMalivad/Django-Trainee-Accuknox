c8339ac58350579ce084cfd893650c21bc473aaa
AmitMalivad <amitmalivad701@gmail.com>
2025-08-22 17:39:51 +0530

    Do Django signals run in the same thread as the caller

--- Testapp/signals.py
+++ Testapp/signals.py
@@ -3,8 +3,15 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 
+import threading
+
 @receiver(post_save, sender=User)
 def slow_signal_handler(sender, instance, **kwargs):
     print("Signal handler started...")
     time.sleep(5) 
     print("Signal handler finished!")
+
+
+@receiver(post_save, sender=User)
+def thread_checker(sender, instance, **kwargs):
+    print("Signal handler thread:", threading.current_thread().name)
--- Testapp/test.py
+++ Testapp/test.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.models import User
 import time
 
+import threading
+
+
 start = time.time()
 User.objects.create(username="test_user")
 end = time.time()
 
 print(f"Total time taken: {end - start} seconds")
+
+
+print("Caller thread:", threading.current_thread().name)
+User.objects.create(username="thread_test")
