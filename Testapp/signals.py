f6620140ac88bb1e898609e61e26010b267de0a7
AmitMalivad <amitmalivad701@gmail.com>
2025-08-22 17:43:08 +0530

    Django signals run in the same database transaction as the caller

--- Testapp/signals.py
+++ Testapp/signals.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 
 import threading
 
+from django.db import connection
+
+
 @receiver(post_save, sender=User)
 def slow_signal_handler(sender, instance, **kwargs):
     print("Signal handler started...")
@@ -15,3 +18,11 @@ def slow_signal_handler(sender, instance, **kwargs):
 @receiver(post_save, sender=User)
 def thread_checker(sender, instance, **kwargs):
     print("Signal handler thread:", threading.current_thread().name)
+
+
+@receiver(post_save, sender=User)
+def transaction_checker(sender, instance, **kwargs):
+    print("Signal handler running...")
+    cursor = connection.cursor()
+    cursor.execute("SELECT COUNT(*) FROM auth_user WHERE username='txn_test'")
+    print("Row count inside signal:", cursor.fetchone()[0])
--- Testapp/test.py
+++ Testapp/test.py
@@ -3,6 +3,8 @@ import time
 
 import threading
 
+from django.db import transaction
+
 
 start = time.time()
 User.objects.create(username="test_user")
@@ -13,3 +15,13 @@ print(f"Total time taken: {end - start} seconds")
 
 print("Caller thread:", threading.current_thread().name)
 User.objects.create(username="thread_test")
+
+
+try:
+    with transaction.atomic():
+        User.objects.create(username="txn_test")
+        raise Exception("Force rollback!")
+except:
+    pass
+
+print("Row count after rollback:", User.objects.filter(username="txn_test").count())
